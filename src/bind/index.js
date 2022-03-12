156b102ee492ef2a48fbceaed6e08214ba0038ad
lyhhh <17610603221@163.com>
2022-03-12 15:46:48 +0800

    支持 new & 考虑原型链

--- src/bind/index.js
+++ src/bind/index.js
@@ -1,3 +1,5 @@
+var slice = Array.prototype.slice;
+
 function bind(asThis) {
   var args = slice.call(arguments, 1);
 
@@ -6,18 +8,31 @@ function bind(asThis) {
     throw new Error("what is trying to bind is not callable");
   }
 
-  return function () {
+  function resultFn() {
     var args2 = slice.call(arguments, 1);
-    return fn.apply(asThis, args.concat(args2));
-  };
+    return fn.apply(
+      resultFn.prototype.isPrototypeOf(this) ? this : asThis,
+      args.concat(args2)
+    );
+  }
+
+  resultFn.prototype = fn.prototype;
+  return resultFn;
 }
 
 // es6 语法等价写法
 function _bind(asThis, ...args) {
   const fn = this;
-  return function (...aArgs2) {
-    return fn.call(asThis, ...args, ...aArgs2);
-  };
+  function resultFn(...aArgs2) {
+    return fn.call(
+      this instanceof resultFn ? this : asThis,
+      ...args,
+      ...aArgs2
+    );
+  }
+
+  resultFn.prototype = fn.prototype;
+  return resultFn;
 }
 
 module.exports = bind;
