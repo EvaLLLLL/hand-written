f526e0e51f8c090965c1172f9205b1706b217eca
lyhhh <17610603221@163.com>
2022-03-31 14:37:05 +0800

    add promise all & race

---
+++ promise/all&race.js
@@ -0,0 +1,36 @@
+import { MyPromise } from './practice';
+
+const myAll = (iterator) => {
+	let currentCount = 0
+	let len = iterator.length
+	let result = []
+	
+	return new Promise((resolve, reject) => {
+		for(let key in iterator) {
+			Promise.resolve(iterator[key])
+				.then(data => {
+					result[key] = data
+					
+					if (currentCount === len) {
+						resolve(result)
+					}
+					
+					currentCount += 1
+				})
+				.catch(e => reject(e))
+		}
+	})
+}
+
+const myRace = (iterator) => {
+	return new Promise((resolve, reject) => {
+		for(let i in iterator) {
+			Promise.resolve(i)
+				.then(data => resolve(data))
+				.catch(e => reject(e))
+		}
+	})
+}
+
+MyPromise.prototype.all = myAll
+MyPromise.prototype.race = myRace
