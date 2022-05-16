0222c3be277083cd04c9915e847244a657bcdebd
lyhhh <17610603221@163.com>
2022-05-16 14:58:58 +0800

    update

---
+++ instanceof/practice.js
@@ -0,0 +1,16 @@
+function myInstanceof(obj, target) {
+  while (true) {
+    if (!obj.__proto__) return false
+
+    if (obj.__proto__ === target.prototype) {
+      return true
+    }
+
+    obj = obj.__proto__
+  }
+}
+
+console.log(myInstanceof({}, Object))
+console.log(myInstanceof(2, Number))
+console.log(myInstanceof('', String))
+console.log(myInstanceof('1', String))
