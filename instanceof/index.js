447455ccabf8da47c9f341cf5a02d804abc8de74
lyhhh <17610603221@163.com>
2022-05-16 15:59:22 +0800

    update

--- instanceof/index.js
+++ instanceof/index.js
@@ -1,15 +1,19 @@
-function myInstanceOf(target, asConstructor) {
+function myInstanceof(target, asConstructor) {
   while (true) {
-    if (target === null) {
+    if (target['__proto__'] === null) {
       return false
     }
 
-    if (target.__proto__ === asConstructor.prototype) {
+    if (target['__proto__'] === asConstructor.prototype) {
       return true
     }
 
-    target = target.__proto__
+    target = target['__proto__']
   }
 }
 
-console.log(myInstanceOf(() => {}, Function))
+console.log(myInstanceof(() => {}, Function))
+console.log(myInstanceof({}, Object))
+console.log(myInstanceof(2, Number))
+console.log(myInstanceof('', String))
+console.log(myInstanceof('1', String))
--- instanceof/practice.js
+++ instanceof/practice.js
@@ -1,12 +1,12 @@
 function myInstanceof(obj, target) {
   while (true) {
-    if (!obj.__proto__) return false
+    if (obj['__proto__'] === null) return false
 
-    if (obj.__proto__ === target.prototype) {
+    if (obj['__proto__'] === target.prototype) {
       return true
     }
 
-    obj = obj.__proto__
+    obj = obj['__proto__']
   }
 }
 
